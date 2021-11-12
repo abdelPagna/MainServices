14680369fa484321c37da52aab6c56ba1e0ee48f
regab <mgbachris@gmail.com>
2021-11-12 12:50:18 +0100

    ok

--- src/app/components/shared/header-two/header-two.component.ts
+++ src/app/components/shared/header-two/header-two.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HelperService } from '../../helper/helper.service';
-import data from "../../data/navigation.json";
+import data from '../../data/navigation.json';
 
 @Component({
   selector: 'app-header-two',
